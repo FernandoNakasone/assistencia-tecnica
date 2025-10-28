32f4186e70bab588ed76b639e4d58a6c3e4c42f8
FernandoNakasone <fernandonakasone040@gmail.com>
2025-10-28 09:33:06 -0300

    Continuação do ex assistencia tecnica metodos de inserção e busca no banco de dados oracle  sql

--- src/main/java/br/fiap/assistencia_tecnica/web/controller/ClienteController.java
+++ src/main/java/br/fiap/assistencia_tecnica/web/controller/ClienteController.java
@@ -6,6 +6,8 @@ import br.fiap.assistencia_tecnica.web.dto.ClienteRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
@@ -28,5 +30,15 @@ public class ClienteController {
         cliente.setSenha(requesicao.getSenha());
 
         return service.salvar(cliente);
+    }
+
+    @GetMapping
+    public List<Cliente> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente buscaPorId(@PathVariable Long id) {
+        return service.buscaPorId(id);
     }
 }
